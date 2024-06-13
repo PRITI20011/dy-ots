31be835b8da50d5997cee6be6d4ce7335210564e
Ashokkamble <ashokkamble1195@gmail.com>
2024-06-13 11:46:01 +0530

    create a branch

---
+++ app/Http/Controllers/MobileAppDevelopmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MobileAppDevelopmentController extends Controller
+{
+    public function index(){
+        return view('pages.Mobile-App-DeveopmentController');
+    }
+    
+}
